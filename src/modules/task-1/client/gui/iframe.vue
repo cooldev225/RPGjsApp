ad7855f006eb4f63c42316befcf1141c058d4e09
cooldev225 <goldstar22501@gmail.com>
2022-07-29 09:33:59 +0900

    update webpage instead of simple map in iframe

--- src/modules/task-1/client/gui/iframe.vue
+++ src/modules/task-1/client/gui/iframe.vue
@@ -13,7 +13,7 @@
                 title="Inline Frame Example"
                 width="100%"
                 height="100%"
-                src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+                src="https://www.ubereats.com/">
                 </iframe>
         </div>
     </rpg-window>
